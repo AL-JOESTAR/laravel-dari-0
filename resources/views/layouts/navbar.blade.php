eaa8d3909b841e75dc2dc9a670a6e55cc6d2b4a0
Sainzel <cppnaf@gmail.com>
2026-03-04 23:38:23 +0700

    edit welcome

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">ini navbar</a>
+    <a class="navbar-brand" href="/">ini navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Catatan HTML Dasar</title>
-  <link rel="stylesheet" href="main.css">
-</head>
-<body>
-  <!-- Judul -->
-  <h1>Ini untuk judul 1 (paling besar)</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('konten')
+  <h1>aman aja gess</h1>
+@endsection
