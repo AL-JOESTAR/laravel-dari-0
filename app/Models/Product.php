9ac0b73fd3d09f9d81d30182ea146da2021e370b
Sainzel <cppnaf@gmail.com>
2026-03-05 17:45:31 +0700

    jenis jenis migaration

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //inisialisasi tabel
+    protected $table = 'tb_product';
+
+    //inisialisasi promarykeydaam tabel
+    protected $primarykey = 'id_prodcut';
+
+    //mengatur atau menentukan satu per satu kolom yang bisa di isi
+    // protected $fillable = ['nama_product','harga','stock'];
+
+    //inisialisasi data apa saja yang tidak boleh di isi
+    protected $guraded = ['id_product'];
+
+}
